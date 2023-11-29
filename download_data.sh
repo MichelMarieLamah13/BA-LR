9eb8bb7d1ea98c4a45380d28c827852d9a134297
mmlamah <lamahmichelmarie@gmail.com>
2023-11-29 12:13:43 +0100

    29 nov 2023 12:13

--- download_data.sh
+++ download_data.sh
@@ -3,17 +3,17 @@
 # Copyright (c) 2023. Imen Ben Amor
 #
 
-#SBATCH --job-name=ba_lr
+#SBATCH --job-name=exp
 #SBATCH --partition=gpu
 #SBATCH --time=7-00:00:00
-#SBATCH --output=ba_lr_output.log
-#SBATCH --error=ba_lr_error.log
-#SBATCH --mem=32G
+#SBATCH --output=output.log
+#SBATCH --error=error.log
+# # #SBATCH --mem=32G
 
 
 source /etc/profile.d/conda.sh
 conda activate ba_lr
 
-wget "https://filesender.renater.fr/download.php?token=66c1be29-2f57-476a-be3a-ce470c437a54&files_ids=30866254"
+cp -r /local_disk/clytie/ibenamor/phd_experiments/data/Explainability/BA/* ./data/
 
 conda deactivate
