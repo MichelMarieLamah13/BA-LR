9164720c2a511574a0cffd84125ef27a89de9463
miguel <lamahmichelmarie@gmail.com>
2023-12-01 16:33:31 +0100

    01 dec 2023 16:33

--- exp.sh
+++ exp.sh
@@ -4,8 +4,8 @@
 #
 
 #SBATCH --job-name=exp
-# #SBATCH --partition=gpu
-# #SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
 #SBATCH --time=7-00:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --output=output.log
@@ -18,8 +18,8 @@ conda activate ba_lr
 
 
 # cp -r /local_disk/clytie/ibenamor/phd_experiments/data/Explainability/BA/* ./data/BA/
-# python3 Step3/attribute_explainer.py
-python3 fix_problem.py
+python3 Step3/attribute_explainer.py
+# python3 fix_problem.py
 # python3 correct_files.py
 
 conda deactivate
