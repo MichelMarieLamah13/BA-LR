7a878a93b8e1fb1268bc6a3d11a7446051a79061
mmlamah <lamahmichelmarie@gmail.com>
2023-12-01 15:07:52 +0100

    01 dec 2023 15:07

--- exp.sh
+++ exp.sh
@@ -4,10 +4,10 @@
 #
 
 #SBATCH --job-name=exp
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+# #SBATCH --partition=gpu
+# #SBATCH --gres=gpu:1
 #SBATCH --time=7-00:00:00
-#SBATCH --cpus-per-task=4
+# #SBATCH --cpus-per-task=4
 #SBATCH --output=output.log
 #SBATCH --error=error.log
 #SBATCH --mem=32G
@@ -17,8 +17,8 @@ source /etc/profile.d/conda.sh
 conda activate ba_lr
 
 
-
-python3 Step3/attribute_explainer.py
+cp -r /local_disk/clytie/ibenamor/phd_experiments/data/Explainability/BA/* ./data/BA/
+# python3 Step3/attribute_explainer.py
 # python3 fix_problem.py
 # python3 correct_files.py
 
