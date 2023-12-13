8a848db1cfb5bbadcd61bf712d506531260d07eb
miguel <lamahmichelmarie@gmail.com>
2023-12-13 08:52:35 +0100

    13 Dec 2023 08:52

--- Step3/attribute_explainer_lime.py
+++ Step3/attribute_explainer_lime.py
@@ -1,27 +1,17 @@
 # ==============================================================================
 #  Copyright (c) 2023. Imen Ben Amor
 # ==============================================================================
-import pdb
+import os
 import pickle
 import sys
 
-import numpy as np
 import pandas as pd
-from tqdm import tqdm
-from sklearn.ensemble import GradientBoostingClassifier
-
-from sklearn import tree
-from sklearn.model_selection import GridSearchCV
-import shap
-import os
-
-from attribute_explainer import prepare_data
-from build_contributions import *
-from plots import *
-from test import *
-import lime
 from lime import lime_tabular
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+
+from test import *
 
 
 def save_explanation(explanation, ba, idx):
@@ -38,6 +28,19 @@ def load_explanation(ba, idx):
         return explanation
 
 
+def prepare_data(ba, mloc_train, floc_train):
+    logging.info(f'Number of men in Train={mloc_train}')
+    logging.info(f'Number of female in Train={floc_train}')
+    ba0 = pd.read_csv(f"data/BA/{ba}_0.csv")
+    ba1 = pd.read_csv(f"data/BA/{ba}_1.csv")
+    X = pd.concat([ba0, ba1], ignore_index=True)
+    S = StandardScaler()
+    X_scaled = pd.DataFrame(S.fit_transform(X.iloc[:, 1:]), columns=X.iloc[:, 1:].columns)
+    X_scaled.insert(0, 'name', X["name"])
+    y = [0] * len(ba0) + [1] * len(ba1)
+    return X_scaled, y, ba0, ba1
+
+
 def lime_tabular_explainer():
     meta_vox2 = pd.read_csv("data/vox2_meta.csv")
     floc_train = meta_vox2[meta_vox2["Set"] == "dev"]["Gender"].to_list().count("f")
@@ -50,7 +53,7 @@ def lime_tabular_explainer():
             print(f"BEGIN preparing data: {ba}")
             sys.stdout.flush()
             X, y, ba0, ba1 = prepare_data(ba, mloc_train, floc_train)
-            input_features = X.columns[:-1].to_list()
+            input_features = X.columns[1:].to_list()
             X = X[input_features]
             print(f"END preparing data: {ba}")
             sys.stdout.flush()
@@ -75,6 +78,7 @@ def lime_tabular_explainer():
 
             print(f"BEGIN explaining model: {ba}")
             sys.stdout.flush()
+
             for idx, row in X_test.iterrows():
                 print(f"{ba} - {idx}")
                 sys.stdout.flush()
@@ -85,5 +89,19 @@ def lime_tabular_explainer():
             sys.stdout.flush()
 
 
+def save_explained_data():
+    meta_vox2 = pd.read_csv("data/vox2_meta.csv")
+    floc_train = meta_vox2[meta_vox2["Set"] == "dev"]["Gender"].to_list().count("f")
+    mloc_train = meta_vox2[meta_vox2["Set"] == "dev"]["Gender"].to_list().count("m")
+    BA = ['BA2', 'BA3', 'BA4', 'BA5', 'BA8', 'BA9', 'BA10']
+    for ba in tqdm(BA):
+        if os.path.isfile(f"data/BA/{ba}_0.csv"):
+            X, y, _, _ = prepare_data(ba, mloc_train, floc_train)
+            _, X_test, _, y_test = train_test_split(X, y, test_size=2, random_state=0)
+            X_test['y'] = y_test
+            X_test.to_csv(f'Step3/explainability_results/lime/{ba}/explain_data.csv')
+
+
 if __name__ == "__main__":
-    lime_tabular_explainer()
+    # lime_tabular_explainer()
+    save_explained_data()
