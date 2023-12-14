5955d6881951450d4b5c49eb7b5186cfd89e4c16
miguel <lamahmichelmarie@gmail.com>
2023-12-14 22:59:13 +0100

    14 Dec 2023 22:59

--- Step3/attribute_explainer_lime.py
+++ Step3/attribute_explainer_lime.py
@@ -60,11 +60,9 @@ def lime_tabular_explainer():
 
             print(f"BEGIN training model: {ba}")
             sys.stdout.flush()
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=2, random_state=0)
-            model = GradientBoostingClassifier(
-                max_depth=2,
-                random_state=0,
-                n_estimators=10)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=10, random_state=0)
+            save_explained_data(ba, X_test.copy(), y_test)
+            model = GradientBoostingClassifier()
             model.fit(X=X_train, y=y_train)
             explainer = lime_tabular.LimeTabularExplainer(
                 X_train.values,
@@ -82,26 +80,16 @@ def lime_tabular_explainer():
             for idx, row in X_test.iterrows():
                 print(f"{ba} - {idx}")
                 sys.stdout.flush()
-                explanation = explainer.explain_instance(row, model.predict_proba,
-                                                         num_features=10)
+                explanation = explainer.explain_instance(row, model.predict_proba, num_features=10)
                 save_explanation(explanation, ba, idx)
             print(f"END explaining model: {ba}")
             sys.stdout.flush()
 
 
-def save_explained_data():
-    meta_vox2 = pd.read_csv("data/vox2_meta.csv")
-    floc_train = meta_vox2[meta_vox2["Set"] == "dev"]["Gender"].to_list().count("f")
-    mloc_train = meta_vox2[meta_vox2["Set"] == "dev"]["Gender"].to_list().count("m")
-    BA = ['BA2', 'BA3', 'BA4', 'BA5', 'BA8', 'BA9', 'BA10']
-    for ba in tqdm(BA):
-        if os.path.isfile(f"data/BA/{ba}_0.csv"):
-            X, y, _, _ = prepare_data(ba, mloc_train, floc_train)
-            _, X_test, _, y_test = train_test_split(X, y, test_size=2, random_state=0)
-            X_test['y'] = y_test
-            X_test.to_csv(f'Step3/explainability_results/lime/{ba}/explain_data.csv')
+def save_explained_data(ba, X_test, y_test):
+    X_test['y'] = y_test
+    X_test.to_csv(f'Step3/explainability_results/lime/{ba}/explain_data.csv')
 
 
 if __name__ == "__main__":
-    # lime_tabular_explainer()
-    save_explained_data()
+    lime_tabular_explainer()
